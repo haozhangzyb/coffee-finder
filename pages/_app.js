c9ab24c465c2de40a44346695a6eaf7e72ca3a36
Hao Zhang <haozhangzyb@outlook.com>
2023-04-27 12:24:32 -0400

    change font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import { IBM_Plex_Sans } from "next/font/google";
+
+const ibm_plex_sans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={ibm_plex_sans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
