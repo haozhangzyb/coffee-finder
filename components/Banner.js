85e20281ae9032455526afe6091db0d0b88fba65
Hao Zhang <haozhangzyb@outlook.com>
2023-04-26 20:05:22 -0400

    initi project and add baner

---
+++ components/Banner.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "@/styles/Banner.module.css";
+
+const Banner = ({ findLocalStoreButtonText, handleOnClick }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee </span>
+        <span className={styles.title2}>Finder</span>
+      </h1>
+      <p className={styles.subTitle}>Discover your local coffee shop</p>
+      <button className={styles.button} onClick={handleOnClick}>
+        {findLocalStoreButtonText}
+      </button>
+    </div>
+  );
+};
+
+export default Banner;
